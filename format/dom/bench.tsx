04be06688c1d30d212027fe4577bebc4c2735870
daniellacosse <3759828+daniellacosse@users.noreply.github.com>
2026-04-16 19:12:19 -0400

    wip: add server-side react comparison

---
+++ format/dom/bench.tsx
@@ -0,0 +1,41 @@
+/** @jsxImportSource @cutout/jsx */
+
+import { Window } from "happy-dom";
+import { wikipediaOrg } from "../constants/wikipedia.tsx";
+import { dom } from "./main.ts";
+
+Object.assign(globalThis, { document: new Window().document });
+
+Deno.bench(
+  `format/dom via happy-dom - wikipedia.org`,
+  { group: "wikipedia.org (no style/script tags)" },
+  () => {
+    dom(wikipediaOrg())[0].outerHTML;
+  },
+);
+
+Deno.bench(
+  `format/dom via happy-dom - 10000 rows`,
+  { group: "10000 rows" },
+  (bench) => {
+    const rows = Array.from({ length: 10000 }, (_, i) => ({
+      id: `row-${i}`,
+      class: i % 2 === 0 ? "even" : "odd",
+      content: `Row #${i}`,
+    }));
+
+    bench.start();
+
+    dom(
+      <div>
+        {rows.map((row) => (
+          <div id={row.id} class={row.class}>
+            {row.content}
+          </div>
+        ))}
+      </div>,
+    )[0].outerHTML;
+
+    bench.end();
+  },
+);
---
+++ format/html/bench.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @cutout/jsx */
+
+import { wikipediaOrg } from "../constants/wikipedia.tsx";
+import { html } from "./main.ts";
+
+Deno.bench(
+  `format/html - wikipedia.org`,
+  { group: "wikipedia.org (no style/script tags)" },
+  () => {
+    html(wikipediaOrg());
+  },
+);
+
+Deno.bench(`format/html - 10000 rows`, { group: "10000 rows" }, (bench) => {
+  const rows = Array.from({ length: 10000 }, (_, i) => ({
+    id: `row-${i}`,
+    class: i % 2 === 0 ? "even" : "odd",
+    content: `Row #${i}`,
+  }));
+
+  bench.start();
+
+  html(
+    <div>
+      {rows.map((row) => (
+        <div id={row.id} class={row.class}>
+          {row.content}
+        </div>
+      ))}
+    </div>,
+  );
+
+  bench.end();
+});
