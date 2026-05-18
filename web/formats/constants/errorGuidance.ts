9f99ad888754005a9b62f6a8e9eb3c1a288a91c8
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2026-05-18 19:19:46 -0400

    need a cleanup here

--- web/formats/constants/errorGuidance.ts
+++ web/formats/constants/errorGuidance.ts
@@ -1,2 +1,4 @@
+// TODO(#): reorganize the web/formats' constants and types
+
 export const FUNCTION_SERIALIZATION =
   "Consider writing a custom format or transforming this function into a securely serializable value.";
