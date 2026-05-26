d848791019c86c16f46fa37371bab073fed96f06
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2026-05-26 10:51:26 -0400

    restore "bench" - it's actually appropriate here

---
+++ web/.bench/bench.tsx
@@ -0,0 +1,7 @@
+// deno-lint-ignore-file import-order/import-order
+
+import "./html.benchControl.jsx";
+import "../core/format/dom/benchCompare.tsx";
+
+import "./dom.benchControl.jsx";
+import "../core/format/html/benchCompare.tsx";
